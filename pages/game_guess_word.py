8a109f0eff9e4c5f0e1069062c1f056363d68749
cheerzhang <zhanglenus@gmail.com>
2023-06-27 21:12:27 +0200

    Update game_guess_word.py

--- pages/game_guess_word.py
+++ pages/game_guess_word.py
@@ -1,56 +1,81 @@
 import streamlit as st
-import random
 
-# Define the list of words for the game
-words = ["apple", "banana", "orange", "watermelon", "strawberry", "grapefruit"]
+# Initialize the Tic-Tac-Toe board
+board = [['', '', ''],
+         ['', '', ''],
+         ['', '', '']]
 
-# Select a random word from the list
-word = random.choice(words)
+# Define the player markers
+player1_marker = 'X'
+player2_marker = 'O'
 
-# Initialize the game state
-guessed_letters = []
-num_attempts = 6
+# Define the current player
+current_player = player1_marker
 
 # Main game logic
 def main():
-    st.title("Hangman")
-    st.write("Guess the word!")
+    st.title("Tic-Tac-Toe")
+    st.write("Player 1: X")
+    st.write("Player 2: O")
+    st.write("Let's play!")
 
-    # Display the current game state
-    display_word = "".join([letter if letter in guessed_letters else "_" for letter in word])
-    st.write(display_word)
+    # Display the Tic-Tac-Toe board
+    for row in board:
+        st.write(row)
 
-    # Let the player enter a letter
-    letter = st.text_input("Enter a letter").lower()
+    # Let the players make their moves
+    while True:
+        row = st.number_input("Enter the row (0, 1, or 2):", min_value=0, max_value=2, step=1)
+        column = st.number_input("Enter the column (0, 1, or 2):", min_value=0, max_value=2, step=1)
 
-    # Validate the input
-    if not letter.isalpha() or len(letter) != 1:
-        st.warning("Please enter a single letter.")
-        return
+        # Check if the selected position is valid
+        if board[row][column] != '':
+            st.warning("Invalid move. Please select an empty position.")
+            continue
 
-    # Check if the letter has already been guessed
-    if letter in guessed_letters:
-        st.warning("You have already guessed that letter.")
-        return
+        # Make the move
+        board[row][column] = current_player
 
-    # Update the game state
-    guessed_letters.append(letter)
+        # Check if the current player has won
+        if check_winner(current_player):
+            st.write(f"Player {current_player} wins!")
+            break
 
-    # Check if the letter is in the word
-    if letter in word:
-        st.success("Correct guess!")
-    else:
-        st.error("Wrong guess!")
-        num_attempts -= 1
+        # Check if it's a draw
+        if check_draw():
+            st.write("It's a draw!")
+            break
 
-    # Check if the game is over
-    if num_attempts == 0:
-        st.write("Game over! You lost.")
-        st.write(f"The word was: {word}")
-    elif all(letter in guessed_letters for letter in word):
-        st.write("Congratulations! You won.")
-    else:
-        st.write(f"You have {num_attempts} attempts left.")
+        # Switch to the next player
+        current_player = player2_marker if current_player == player1_marker else player1_marker
+
+# Function to check if a player has won
+def check_winner(player):
+    # Check rows
+    for row in board:
+        if row.count(player) == 3:
+            return True
+
+    # Check columns
+    for col in range(3):
+        if [board[row][col] for row in range(3)].count(player) == 3:
+            return True
+
+    # Check diagonals
+    if board[0][0] == board[1][1] == board[2][2] == player:
+        return True
+
+    if board[0][2] == board[1][1] == board[2][0] == player:
+        return True
+
+    return False
+
+# Function to check if it's a draw
+def check_draw():
+    for row in board:
+        if '' in row:
+            return False
+    return True
 
 # Run the game
 if __name__ == "__main__":
