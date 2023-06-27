6a9136fee5c19b56fd71d608e72f900a010f3a5b
cheerzhang <zhanglenus@gmail.com>
2023-06-27 14:09:18 +0200

    Update dutch.py

--- pages/dutch.py
+++ pages/dutch.py
@@ -13,21 +13,23 @@ words = article.split()
 st.write(article)
 
 # Select a word using a selectbox
-selected_word = st.selectbox("Select a word", words)
+selected_word = st.selectbox("Select a word", [''] + words)
 
 # Enter a memo for the selected word
 memo = st.text_input("Enter a memo")
 
-# Store the selected word and memo in a dictionary
-word_memo = {selected_word: memo}
+# Button to trigger saving the word-memo pair
+if st.button("Save"):
+    # Store the selected word and memo in a dictionary
+    word_memo = {selected_word: memo}
 
-# Store the word-memo pairs in a list
-selected_words = st.session_state.get('selected_words', [])
-selected_words.append(word_memo)
-st.session_state.selected_words = selected_words
+    # Store the word-memo pairs in a list
+    selected_words = st.session_state.get('selected_words', [])
+    selected_words.append(word_memo)
+    st.session_state.selected_words = selected_words
 
 # Display the selected words and memos
 st.write("Selected words:")
-for item in selected_words:
+for item in st.session_state.selected_words:
     word, memo = next(iter(item.items()))
     st.write(f"{word}: {memo}")
