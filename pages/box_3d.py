8095ced6a2b0e24d5e5b429aa57fe5303ec44b40
cheerzhang <zhanglenus@gmail.com>
2023-06-27 22:53:05 +0200

    Update box_3d.py

--- pages/box_3d.py
+++ pages/box_3d.py
@@ -6,63 +6,44 @@ room_width = 6
 room_height = 3
 room_length = 8
 
-# Create a 3D room figure
-fig = go.Figure()
-
-# Add room walls
-fig.add_trace(go.Mesh3d(
-    x=[0, room_length, room_length, 0, 0, 0, room_length, room_length, room_length, room_length, 0, 0],
-    y=[0, 0, room_height, room_height, 0, 0, 0, room_height, room_height, 0, 0, room_height],
-    z=[0, 0, 0, 0, 0, room_width, room_width, room_width, 0, 0, 0, 0],
-    color='lightgray',
-    opacity=0.7
-))
-
-# Add a table in the room
-table_width = 2
-table_height = 0.8
-table_length = 4
-table_x = (room_length - table_length) / 2
-table_y = (room_height - table_height) / 2
-table_z = (room_width - table_width) / 2
-
-fig.add_trace(go.Mesh3d(
-    x=[table_x, table_x + table_length, table_x + table_length, table_x, table_x, table_x, table_x + table_length, table_x + table_length, table_x + table_length, table_x + table_length, table_x, table_x],
-    y=[table_y, table_y, table_y + table_height, table_y + table_height, table_y, table_y, table_y, table_y + table_height, table_y + table_height, table_y, table_y, table_y + table_height],
-    z=[table_z, table_z, table_z, table_z, table_z, table_z + table_width, table_z + table_width, table_z + table_width, table_z, table_z, table_z, table_z],
-    color='brown',
-    opacity=0.7
-))
-
-# Add table legs
-table_leg_height = table_height / 2
-table_leg_width = table_width / 10
-table_leg_length = table_length / 10
-table_leg_x = [table_x, table_x + table_length, table_x, table_x + table_length]
-table_leg_y = [table_y, table_y, table_y + table_height, table_y + table_height]
-table_leg_z = [table_z, table_z, table_z, table_z]
-
-for i in range(4):
-    fig.add_trace(go.Mesh3d(
-        x=[table_leg_x[i], table_leg_x[i], table_leg_x[i] + table_leg_width, table_leg_x[i] + table_leg_width, table_leg_x[i]],
-        y=[table_leg_y[i], table_leg_y[i] + table_leg_length, table_leg_y[i] + table_leg_length, table_leg_y[i], table_leg_y[i]],
-        z=[table_leg_z[i], table_leg_z[i], table_leg_z[i], table_leg_z[i], table_leg_z[i]],
-        color='black',
+# Create room walls
+room_walls = [
+    go.Mesh3d(
+        x=[0, room_length, room_length, 0, 0, room_length, room_length, 0],
+        y=[0, 0, room_height, room_height, 0, 0, room_height, room_height],
+        z=[0, 0, 0, 0, room_width, room_width, room_width, room_width],
+        color='lightgray',
         opacity=0.7
-    ))
+    )
+]
 
-# Set figure layout
-fig.update_layout(
+# Create a chair
+chair = go.Mesh3d(
+    x=[2, 3, 3, 2, 2, 3, 3, 2, 2.5, 2.5, 3, 3],
+    y=[1, 1, 2, 2, 1, 1, 2, 2, 1, 2, 1, 2],
+    z=[1, 1, 1, 1, 0.5, 0.5, 0.5, 0.5, 0, 0, 0, 0],
+    color='red',
+    opacity=0.7
+)
+
+# Create the 3D layout
+layout = go.Layout(
     scene=dict(
         xaxis=dict(visible=False),
         yaxis=dict(visible=False),
         zaxis=dict(visible=False),
-        aspectratio=dict(x=1, y=1, z=1),
-        camera_eye=dict(x=1.5, y=-1.5, z=0.8),
-        annotations=[]
+        aspectmode="manual",
+        aspectratio=dict(x=room_length, y=room_height, z=room_width)
     ),
-    scene_camera=dict(up=dict(x=0, y=0, z=1))
+    margin=dict(r=0, l=0, b=0, t=0)
 )
 
-# Display the 3D room
+# Combine room walls and chair into a data list
+data = room_walls + [chair]
+
+# Create the figure
+fig = go.Figure(data=data, layout=layout)
+
+# Render the figure using Plotly
 st.plotly_chart(fig)
+
