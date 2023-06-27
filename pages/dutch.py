c853fbf7027915b541077e782e612d8db13db19d
cheerzhang <zhanglenus@gmail.com>
2023-06-27 13:48:31 +0200

    Update dutch.py

--- pages/dutch.py
+++ pages/dutch.py
@@ -28,12 +28,13 @@ words = article.split()
 st.write(article)
 
 # Function to save the selected word
-def save_word(word):
-    selected_words = st.session_state.get('selected_words', [])
-    if word not in selected_words:
-        selected_words.append(word)
-        st.session_state.selected_words = selected_words
+for word in words:
+    hover_text = f'<span style="background-color: yellow;">{word}</span>'
+    st.markdown(f'<span title="Click to save" style="cursor: pointer;">{hover_text}</span>', unsafe_allow_html=True)
+    if st.markdown_clicks():
+        save_word(word)
 
-# Loop through the words and add hover functionality
+# Display the selected words
+st.write("Selected words:", st.session_state.selected_words)
 for word in words:
     hover_text = f'<span style="background-color: yellow;">{word}</span>'
