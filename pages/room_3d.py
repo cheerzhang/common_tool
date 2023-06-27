8d49509407fc2994b1ccf34f0a81cdb015eb6999
cheerzhang <zhanglenus@gmail.com>
2023-06-27 22:09:21 +0200

    Update room_3d.py

--- pages/room_3d.py
+++ pages/room_3d.py
@@ -1,12 +1,21 @@
 import streamlit as st
-import pyvista as pv
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-# Create a simple 3D box
-mesh = pv.Box()
+# Create 3D coordinates for the box
+x = [0, 1, 1, 0, 0, 1, 1, 0]
+y = [0, 0, 1, 1, 0, 0, 1, 1]
+z = [0, 0, 0, 0, 1, 1, 1, 1]
 
 # Set up the Streamlit app
 st.title("3D Image Viewer")
-st.write("Displaying a 3D box")
+st.write("Displaying a 3D box using Matplotlib")
 
-# Render the 3D image
-st.pyvista_plot(mesh)
+# Create a 3D plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_trisurf(x, y, z, linewidth=0.2, edgecolor='black')
+
+# Show the plot in Streamlit
+st.pyplot(fig)
+
