043c9438d9f14a2a63746ac09fe975d697180360
cheerzhang <zhanglenus@gmail.com>
2023-06-27 22:30:06 +0200

    Update box_3d.py

--- pages/box_3d.py
+++ pages/box_3d.py
@@ -1,12 +1,36 @@
 import streamlit as st
-import pyvista as pv
+import plotly.graph_objects as go
 
-# Create a simple 3D box
-mesh = pv.Box()
+# Create room dimensions
+room_width = 6
+room_height = 3
+room_length = 8
 
-# Set up the Streamlit app
-st.title("3D Image Viewer")
-st.write("Displaying a 3D box")
+# Create a 3D room figure
+fig = go.Figure()
 
-# Render the 3D image
-st.pyvista_plot(mesh)
+# Add room walls
+fig.add_trace(go.Mesh3d(
+    x=[0, room_length, room_length, 0, 0, 0, room_length, room_length, room_length, room_length, 0, 0],
+    y=[0, 0, room_height, room_height, 0, 0, 0, room_height, room_height, 0, 0, room_height],
+    z=[0, 0, 0, 0, 0, room_width, room_width, room_width, 0, 0, 0, 0],
+    color='lightgray',
+    opacity=0.7
+))
+
+# Set figure layout
+fig.update_layout(
+    scene=dict(
+        xaxis=dict(visible=False),
+        yaxis=dict(visible=False),
+        zaxis=dict(visible=False),
+        aspectratio=dict(x=1, y=1, z=1),
+        camera_eye=dict(x=1.5, y=-1.5, z=0.8),
+        annotations=[]
+    ),
+    scene_camera=dict(up=dict(x=0, y=0, z=1))
+)
+
+# Display the 3D room
+st.plotly_chart(fig)
+
