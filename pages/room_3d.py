d34388d5a3fd6b446f51837f96571ec6747270e9
cheerzhang <zhanglenus@gmail.com>
2023-06-28 13:03:20 +0200

    Update room_3d.py

--- pages/room_3d.py
+++ pages/room_3d.py
@@ -1,89 +1,54 @@
 import streamlit as st
-import pythreejs as p3
+import plotly.graph_objects as go
 
 # Create room dimensions
-room_width = 6
+room_width = 4
+room_length = 5
 room_height = 3
-room_length = 8
 
-# Create a 3D scene
-scene = p3.Scene()
-
-# Add room walls
-room_walls = p3.Mesh(
-    p3.BoxGeometry(room_length, room_height, room_width),
-    p3.MeshLambertMaterial(color='lightgray', opacity=0.7, transparent=True),
-    position=[room_length / 2, room_height / 2, room_width / 2]
-)
-scene.add(room_walls)
-
-# Add a table in the room
-table_width = 2
-table_height = 0.8
-table_length = 4
-table_x = (room_length - table_length) / 2
-table_y = (room_height - table_height) / 2
-table_z = (room_width - table_width) / 2
-
-table = p3.Mesh(
-    p3.BoxGeometry(table_length, table_height, table_width),
-    p3.MeshLambertMaterial(color='brown', opacity=0.7, transparent=True),
-    position=[table_x + table_length / 2, table_y + table_height / 2, table_z + table_width / 2]
-)
-scene.add(table)
-
-# Add table legs
-table_leg_height = table_height / 2
-table_leg_width = table_width / 10
-table_leg_length = table_length / 10
-
-table_legs = [
-    p3.Mesh(
-        p3.BoxGeometry(table_leg_width, table_leg_length, table_leg_height),
-        p3.MeshLambertMaterial(color='black', opacity=0.7, transparent=True),
-        position=[
-            table_x + table_length / 2,
-            table_y + table_leg_length / 2,
-            table_z + table_width / 2 - table_leg_width / 2
-        ]
-    ),
-    p3.Mesh(
-        p3.BoxGeometry(table_leg_width, table_leg_length, table_leg_height),
-        p3.MeshLambertMaterial(color='black', opacity=0.7, transparent=True),
-        position=[
-            table_x + table_length / 2,
-            table_y + table_height - table_leg_length / 2,
-            table_z + table_width / 2 - table_leg_width / 2
-        ]
-    ),
-    p3.Mesh(
-        p3.BoxGeometry(table_leg_width, table_leg_length, table_leg_height),
-        p3.MeshLambertMaterial(color='black', opacity=0.7, transparent=True),
-        position=[
-            table_x + table_length / 2 - table_leg_width / 2,
-            table_y + table_leg_length / 2,
-            table_z + table_width / 2 + table_leg_width / 2
-        ]
-    ),
-    p3.Mesh(
-        p3.BoxGeometry(table_leg_width, table_leg_length, table_leg_height),
-        p3.MeshLambertMaterial(color='black', opacity=0.7, transparent=True),
-        position=[
-            table_x + table_length / 2 - table_leg_width / 2,
-            table_y + table_height - table_leg_length / 2,
-            table_z + table_width / 2 + table_leg_width / 2
-        ]
+# Create room walls
+room_walls = [
+    go.Mesh3d(
+        x=[0, room_width, room_width,   0,           0],
+        y=[0, 0,          room_length,  room_length, 0],
+        z=[0, 0, 0, 0, 0],
+        color='lightgray',
+        opacity=0.7
     )
 ]
 
-for leg in table_legs:
-    scene.add(leg)
+# Create room floor
+room_floor = go.Mesh3d(
+   x=[0, room_width, room_width,   0,           0],
+   y=[0, 0,          room_length,  room_length, 0],
+   z=[0, 0, 0, 0, 0],
+   color='lightgray',
+   opacity=0.7
+)
 
-# Create a 3D renderer
-renderer = p3.Renderer(scene, camera='perspective', width=800, height=600)
+# Combine room walls and roof into a data list
+data = room_walls + [room_floor]
 
-# Display the 3D room
-st.pythreejs(renderer)
+# Create the 3D layout
+layout = go.Layout(
+    scene=dict(
+        xaxis=dict(visible=True),
+        yaxis=dict(visible=True),
+        zaxis=dict(visible=True),
+        camera=dict(
+            eye=dict(x=1, y=1, z=1)  # Set the camera position (adjust the values as needed)
+        ),
+        # aspectmode="manual",
+        # aspectratio=dict(x=room_length, y=room_width, z=room_height)
+    ),
+    margin=dict(r=0, l=0, b=0, t=0)
+)
+
+# Create the figure
+fig = go.Figure(data=data, layout=layout)
+
+# Render the figure using Plotly
+st.plotly_chart(fig)
 
 
 
