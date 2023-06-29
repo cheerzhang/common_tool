3a2dec03d79b3d8ee4205e159eab00b0dcdaee75
cheerzhang <zhanglenus@gmail.com>
2023-06-29 17:44:53 +0200

    Update dutch_article.py

--- pages/dutch_article.py
+++ pages/dutch_article.py
@@ -2,9 +2,5 @@ import streamlit as st
 from annotated_text import annotated_text
 
 annotated_text(
-    "Woningcorporatie ", "Vesteda", 
-    ("verbreekt", "disconnect"),
-    "contact", "met", 
-    ("huurders", "tenants"),
-    "die", "willen", "omkopen",
+   "Woningcorporatie Vesteda ", ("verbreekt", "disconnect"), "contact met huurders die willen 'omkopen'"
 )
