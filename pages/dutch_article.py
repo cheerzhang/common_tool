8ea30dc113104bd6b040590161538ac46efc40c8
cheerzhang <zhanglenus@gmail.com>
2023-06-29 18:27:30 +0200

    Update dutch_article.py

--- pages/dutch_article.py
+++ pages/dutch_article.py
@@ -1,19 +1,22 @@
 import streamlit as st
 import requests
 
-url = "https://text-translator2.p.rapidapi.com/translate"
 
-payload = {
-	"source_language": "en",
-	"target_language": "id",
-	"text": "What is your name?"
-}
+title = st.text_input('Movie title', 'Life of Brian')
+st.write('The current movie title is', title)
+
+url = "https://google-translate1.p.rapidapi.com/language/translate/v2/detect"
+
+'''
+payload = { "q": "English is hard, but detectably so" }
 headers = {
 	"content-type": "application/x-www-form-urlencoded",
-	"X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
-	"X-RapidAPI-Host": "text-translator2.p.rapidapi.com"
+	"Accept-Encoding": "application/gzip",
+	"X-RapidAPI-Key": token,
+	"X-RapidAPI-Host": "google-translate1.p.rapidapi.com"
 }
 
 response = requests.post(url, data=payload, headers=headers)
 
 print(response.json())
+'''
