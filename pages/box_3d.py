10dc343e514de7ad4148c48f7b140a642a2aba75
cheerzhang <zhanglenus@gmail.com>
2023-06-29 11:19:18 +0200

    Update box_3d.py

--- pages/box_3d.py
+++ pages/box_3d.py
@@ -9,8 +9,8 @@ room_height = 3.7
 # Create room walls
 room_walls = [
     go.Mesh3d(
-        x=[0, room_width, room_width,   0,           0],
-        y=[0, 0,          room_length,  room_length, 0],
+        x=[0, 3.54, 3,54,   0,           0],
+        y=[0, 0,    11.79,  11.79, 0],
         z=[0, 0, 0, 0, 0],
         color='lightgray',
         opacity=0.7
