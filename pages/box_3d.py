f7f374b25c2f1a821f1ea866a483425afd161e2e
cheerzhang <zhanglenus@gmail.com>
2023-06-29 11:18:35 +0200

    Update box_3d.py

--- pages/box_3d.py
+++ pages/box_3d.py
@@ -2,9 +2,9 @@ import streamlit as st
 import plotly.graph_objects as go
 
 # Create room dimensions
-room_width = 3.54
-room_height = 2.7
-room_length = 11.79
+room_width = 4
+room_length = 5
+room_height = 3.7
 
 # Create room walls
 room_walls = [
@@ -17,84 +17,154 @@ room_walls = [
     )
 ]
 
-# ------------------  Create 1st floor room ----------------------------
-room_floor_1 = go.Mesh3d(
-    x=[0, room_width, room_width,   0,           0],
-    y=[0, 0,          room_length,  room_length, 0],
-    z=[0, 0, 0, 0, 0],
-    color='lightgray',
-    opacity=0.7
+# ------------------------------- Create room floor -----------------------------
+room_floor = go.Mesh3d(
+   x=[0, 3.54, 3,54,   0,           0],
+   y=[0, 0,    11.79,  11.79, 0],
+   z=[0, 0, 0, 0, 0],
+   color='lightgray',
+   opacity=0.7
 )
-# add 1 st sofa
-sofa_1 = go.Mesh3d(
-    x=[0,    1.8,    1.8,   0,    0],
-    y=[5.99, 5.99,   7.99,  7.99, 5.99],
-    z=[0, 0, 0, 0, 0],
-    color='lightpink',
-    opacity=0.7
+floor_1_wall_1 = go.Mesh3d(
+   x=[0, 2.5, 2.5, 0,   0],
+   y=[3, 3,   3.1, 3.1, 3],
+   z=[0, 0,   2.7, 2.7, 0],
+   color='lightgray',
+   opacity=0.7
 )
-
-# add 1 st toelit
-toelit_1 = go.Mesh3d(
-    x=[0,    0.9,    0.9,   0,    0],
-    y=[1.54, 1.54,   2.65,  2.65, 1.54],
-    z=[0, 0, 0, 0, 0],
-    color='lightblue',
-    opacity=0.7
-)
-toelit_real_wall = go.Mesh3d(
-    x=[0,    0.9,    0.9,   0,    0],
-    y=[1.54, 1.54,   1.55,  1.55, 1.54],
-    z=[0, 0, 2.7, 2.7, 0],
-    color='lightblue',
-    opacity=0.7
-)
-toelit_1_wall_1_config_x = [0,    0.9,    0.9,   0,    0]
-toelit_1_wall_1_config_y = [1.54, 1.54,   1.64,  1.64, 1.54]
-toelit_1_wall = []
-for item in range(0, 27, 1):
-    toelit_1_wall.append(go.Mesh3d(x=toelit_1_wall_1_config_x, y=toelit_1_wall_1_config_y, z=[item*0.1,item*0.1,item*0.1,item*0.1,item*0.1], color='lightblue', opacity=0.7))
-toelit_1_wall_2_config_x = [0,    0.1,    0.1,   0,    0]
-toelit_1_wall_2_config_y = [1.54, 1.54,   2.65,  2.65, 1.54]
-for item in range(0, 27, 1):
-    toelit_1_wall.append(go.Mesh3d(x=toelit_1_wall_2_config_x, y=toelit_1_wall_2_config_y, z=[item*0.1,item*0.1,item*0.1,item*0.1,item*0.1], color='lightblue', opacity=0.7))
-toelit_1_wall_3_config_x = [0,    0.9,    0.9,   0,    0]
-toelit_1_wall_3_config_y = [2.65, 2.65,   2.75,  2.75, 2.65]
-for item in range(0, 27, 1):
-    toelit_1_wall.append(go.Mesh3d(x=toelit_1_wall_3_config_x, y=toelit_1_wall_3_config_y, z=[item*0.1,item*0.1,item*0.1,item*0.1,item*0.1], color='lightblue', opacity=0.7))
-
-
-# create a kitchen
-kitchen = go.Mesh3d(
-    x=[0,    room_width,  room_width,   0,              0],
-    y=[7.99, 7.99,        room_length,  room_length, 7.99],
-    z=[0, 0, 0, 0, 0],
-    color='lightyellow',
-    opacity=0.7
-)
-
-room_1st_floor = [room_floor_1, toelit_1, kitchen, sofa_1] + [toelit_real_wall]
-
-# ------------------------ Create a room 2nd floor -------------------------------
 room_floor_2 = go.Mesh3d(
-    x=[0, room_width, room_width,   0,           0],
-    y=[0, 0,          8.01,         8.01,        0],
-    z=[room_height, room_height, room_height, room_height, room_height],
-    color='lightgray',
-    opacity=0.7
+   x=[0, room_width, room_width,   0,           0],
+   y=[0, 0,          room_length,  room_length, 0],
+   z=[2.7, 2.7, 2.7, 2.7, 2.7],
+   color='lightgray',
+   opacity=0.7
 )
-room_2nd_room_1 = go.Mesh3d(
-    x=[0, room_width, room_width,   0,           0],
-    y=[0, 0,          2.46,         2.46,        0],
-    z=[room_height, room_height, room_height, room_height, room_height],
-    color='lightpink',
-    opacity=0.7
-)
-room_2nd_floor = [room_floor_2, room_2nd_room_1]
+# --------------------------- stair ---------------------------------
 
+
+# --------------------------- create a toilet ----------------------------------
+toilet = go.Mesh3d(
+   x=[3, 4, 4, 3, 3],
+   y=[0, 0, 2, 2, 0],
+   z=[0, 0, 0, 0, 0],
+   color='lightblue',
+   opacity=0.7
+)
+toilet_wall_1 = go.Mesh3d(
+   x=[3, 3.1, 3.1, 3, 3],
+   y=[0, 0.1, 1.9, 2, 0],
+   z=[0, 2.7, 2.7, 0, 0],
+   color='lightblue',
+   opacity=0.7
+)
+toilet_wall_2 = go.Mesh3d(
+   x=[3.9, 4, 4, 3.9, 3.9],
+   y=[0.1, 0, 2, 1.9, 0.1],
+   z=[2.7, 0, 0, 2.7, 2.7],
+   color='lightblue',
+   opacity=0.7
+)
+toilet_wall_3 = go.Mesh3d(
+   x=[3, 4, 3.9, 3.1, 3],
+   y=[0, 0, 0.1, 0.1, 0],
+   z=[0, 0, 2.7, 2.7, 0],
+   color='lightblue',
+   opacity=0.7
+)
+toilet_wall_4 = go.Mesh3d(
+   x=[3, 4, 3.9, 3.1, 3],
+   y=[2, 2, 1.9, 1.9, 2],
+   z=[0, 0, 2.7, 2.7, 0],
+   color='lightblue',
+   opacity=0.7
+)
+toilet_floor = go.Mesh3d(
+   x=[3.1, 3.9, 3.9, 3.1, 3.1],
+   y=[0.1, 0.1, 1.9, 1.9, 0.1],
+   z=[2.7, 2.7, 2.7, 2.7, 2.7],
+   color='lightblue',
+   opacity=0.7
+)
+toilet_ = [toilet, toilet_wall_1, toilet_wall_2, toilet_wall_3, toilet_wall_4]
+
+# ----------------------------  create a bed  ---------------------------------
+bed = go.Mesh3d(
+   x=[0.1,   1.9,   1.9,   0.1,   0.1],
+   y=[0.1,   0.1,   1.7,   1.7,   0.1],
+   z=[0.5,   0.5,   0.5,   0.5,   0.5],
+   color='pink',
+   opacity=0.7
+)
+bed_head = go.Mesh3d(
+   x=[0,   0.1,   0.1,  0,   0],
+   y=[0,   0,     1.8,  1.8, 0],
+   z=[1,   0.5,   0.5,  1,   1],
+   color='pink',
+   opacity=0.7
+)
+bed_leg_1 = go.Mesh3d(
+   x=[0, 2, 1.9, 0.1, 0],
+   y=[0, 0, 0.1, 0.1, 0],
+   z=[0, 0, 0.5, 0.5, 0.5],
+   color='pink',
+   opacity=0.7
+)
+bed_leg_2 = go.Mesh3d(
+   x=[0,   2,   1.9, 0.1,   0],
+   y=[1.8, 1.8, 1.7, 1.7,   1.8],
+   z=[0,   0,   0.5, 0.5,   0.5],
+   color='pink',
+   opacity=0.7
+)
+bed_leg_3 = go.Mesh3d(
+   x=[0,   0.1,   0.1,  0,   0],
+   y=[0,   0.1,   1.7,  1.9,   0],
+   z=[0,   0.5,   0.5,  0,   0],
+   color='pink',
+   opacity=0.7
+)
+bed_leg_4 = go.Mesh3d(
+   x=[1.9, 2,  2,   1.9,   1.9],
+   y=[0.1, 0,  1.8, 1.7,   0.1],
+   z=[0.5, 0,  0,   0.5,   0.5],
+   color='pink',
+   opacity=0.7
+)
+bed_ = [bed, bed_leg_1, bed_leg_2, bed_leg_3, bed_leg_4, bed_head]
+
+# --------------------------- Create a bookshelf -----------------------------
+shelf_1 = go.Mesh3d(
+   x=[2.5, 3, 3, 2.5, 2.5],
+   y=[0,   0,  2,  2,   0],
+   z=[0,   0,  0,  0,   0],
+   color='lightyellow',
+   opacity=0.7
+)
+shelf_2 = go.Mesh3d(
+   x=[2.5, 3,   3,   2.5, 2.5],
+   y=[0,   0,   2,   2,   0],
+   z=[0.5, 0.5, 0.5, 0.5, 0.5],
+   color='lightyellow',
+   opacity=0.7
+)
+shelf_3 = go.Mesh3d(
+   x=[2.5, 3,   3,   2.5, 2.5],
+   y=[0,   0,   2,   2,   0],
+   z=[1,   1,   1,   1,   1],
+   color='lightyellow',
+   opacity=0.7
+)
+shelf_4 = go.Mesh3d(
+   x=[2.5, 3,   3,   2.5, 2.5],
+   y=[0,   0,   2,   2,   0],
+   z=[1.5, 1.5, 1.5, 1.5, 1.5],
+   color='lightyellow',
+   opacity=0.7
+)
+shelves_ = [shelf_1, shelf_2, shelf_3, shelf_4]
 
 # Combine room walls and roof into a data list
-data = room_walls + room_1st_floor + room_2nd_floor
+data = room_walls + [room_floor]
 
 # Create the 3D layout
 layout = go.Layout(
@@ -103,7 +173,7 @@ layout = go.Layout(
         yaxis=dict(visible=True),
         zaxis=dict(visible=True),
         camera=dict(
-            eye=dict(x=1, y=1, z=1)  # Set the camera position (adjust the values as needed)
+            eye=dict(x=0.5, y=0.5, z=1)  # Set the camera position (adjust the values as needed)
         ),
         # aspectmode="manual",
         # aspectratio=dict(x=room_length, y=room_width, z=room_height)
