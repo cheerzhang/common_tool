2f60c57a236526f5288a486e625de6259140fee4
cheerzhang <zhanglenus@gmail.com>
2023-06-29 17:38:54 +0200

    Update dutch_article.py

--- pages/dutch_article.py
+++ pages/dutch_article.py
@@ -3,3 +3,7 @@ import streamlit as st
 highlighted_text = "Hello, **world**!"
 
 st.markdown(f'<span style="background-color: yellow;">{highlighted_text}</span>', unsafe_allow_html=True)
+
+highlighted_text = "Hello, <mark>world</mark>!"
+
+st.markdown(highlighted_text, unsafe_allow_html=True)
