2576ab9efa6be3ad30f539cd755d907974cf2f79
cheerzhang <zhanglenus@gmail.com>
2023-06-29 18:33:38 +0200

    Update dutch_article.py

--- pages/dutch_article.py
+++ pages/dutch_article.py
@@ -5,16 +5,19 @@ import requests
 token = st.text_input('Type in translate API token:', '')
 st.write('The current token used is:', token)
 
-url = "https://google-translate1.p.rapidapi.com/language/translate/v2/detect"
 
-payload = { "q": "English is hard, but detectably so" }
+url = "https://google-translate1.p.rapidapi.com/language/translate/v2"
+payload = {
+	"q": "Hello, world!",
+	"target": "nl",
+	"source": "en"
+}
 headers = {
 	"content-type": "application/x-www-form-urlencoded",
 	"Accept-Encoding": "application/gzip",
-	"X-RapidAPI-Key": token,
+	"X-RapidAPI-Key": "a8f9d97b7emshfb89cd5db62ad47p163116jsn44ffce9232ff",
 	"X-RapidAPI-Host": "google-translate1.p.rapidapi.com"
 }
-
-response = requests.post(url, data=payload, headers=headers)
-
-st.write(response.json())
+if token != "":
+	response = requests.post(url, data=payload, headers=headers)
+	st.write(response.json())
