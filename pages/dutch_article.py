9cf75654a03b41fdbccfd8d46e5b78bce13710d7
cheerzhang <zhanglenus@gmail.com>
2023-06-29 17:43:09 +0200

    Update dutch_article.py

--- pages/dutch_article.py
+++ pages/dutch_article.py
@@ -2,16 +2,9 @@ import streamlit as st
 from annotated_text import annotated_text
 
 annotated_text(
-    "This ",
-    ("is", "verb"),
-    " some ",
-    ("annotated", "adj"),
-    ("text", "noun"),
-    " for those of ",
-    ("you", "pronoun"),
-    " who ",
-    ("like", "verb"),
-    " this sort of ",
-    ("thing", "noun"),
-    "."
+    "Woningcorporatie ", "Vesteda", 
+    ("verbreekt", "disconnect"),
+    "contact", "met", 
+    ("huurders", "tenants"),
+    "die", "willen", "omkopen",
 )
