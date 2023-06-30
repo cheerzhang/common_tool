ca4f71b3c83af4972431874890c7f8e555b02cd5
cheerzhang <zhanglenus@gmail.com>
2023-06-30 14:52:20 +0200

    Update dutch_read.py

--- pages/dutch_read.py
+++ pages/dutch_read.py
@@ -63,4 +63,16 @@ df = pd.DataFrame({
   'willen','Nederland','een','om','in']
 })
 df.to_csv('dutch.csv', index=False)
-st.download_button("Download CSV", "dutch.csv")
+
+@st.cache
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode('utf-8')
+
+csv = convert_df(my_large_df)
+st.download_button(
+    label="Download data as CSV",
+    data=csv,
+    file_name='large_df.csv',
+    mime='text/csv',
+)
