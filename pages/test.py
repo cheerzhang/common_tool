496340ca95ede81032ad1835fcb9ebf3c40b14ae
cheerzhang <zhanglenus@gmail.com>
2023-06-30 17:50:49 +0200

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -1,21 +1,28 @@
 import streamlit as st
 
+@st.cache(allow_output_mutation=True)
+def get_cached_values():
+    return []
+
 def main():
-    st.title("Multi-Select Example")
+    st.title("Caching Selected Values")
+
+    # Retrieve the cached values
+    selected_values = get_cached_values()
 
     options = ['a', 'b', 'c', 'd', 'e']
 
-    selected_indices = st.multiselect("Select values", options)
+    selected_indices = st.multiselect("Select values", options, default=selected_values)
     selected_values = [options[i] for i in selected_indices]
 
-    if st.button("Print Logs"):
-        logs = []
-        for i, value in enumerate(selected_values, start=1):
-            logs.append(f"{i}st: {value}")
-        st.write("Logs:")
-        for log in logs:
-            st.write(log)
+    # Update the cached values
+    get_cached_values().clear()
+    get_cached_values().extend(selected_values)
+
+    if st.button("Print Cached Values"):
+        st.write("Cached Values:", selected_values)
 
 if __name__ == "__main__":
     main()
+
 
