6ffcabc7e8a6218c6c2c7823eb58e5ba3bb9498e
cheerzhang <zhanglenus@gmail.com>
2023-06-30 17:49:34 +0200

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -4,26 +4,18 @@ def main():
     st.title("Multi-Select Example")
 
     options = ['a', 'b', 'c', 'd', 'e']
-    selected_indices = []
 
+    selected_indices = st.multiselect("Select values", options)
     selected_values = [options[i] for i in selected_indices]
-    st.write("Selected Values:", selected_values)
-
-    selected_values_logs = []
-
-    selected = st.multiselect("Select values", options)
-    selected_indices = [options.index(value) for value in selected]
-    selected_values = [options[i] for i in selected_indices]
-
-    selected_values_logs.append(selected_values)
-    selected_values_logs_str = [', '.join(values) for values in selected_values_logs]
-    st.write("Logs:")
-    for i, log in enumerate(selected_values_logs_str):
-        st.write(f"{i+1}st: {log}")
 
     if st.button("Print Logs"):
-        for i, log in enumerate(selected_values_logs_str):
-            st.write(f"{i+1}st: {log}")
+        logs = []
+        for i, value in enumerate(selected_values, start=1):
+            logs.append(f"{i}st: {value}")
+        st.write("Logs:")
+        for log in logs:
+            st.write(log)
 
 if __name__ == "__main__":
     main()
+
