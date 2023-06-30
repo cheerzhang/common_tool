5c07a9aa9aeb9cb959b1fb4996737b3374c904ab
cheerzhang <zhanglenus@gmail.com>
2023-06-30 17:54:30 +0200

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -1,26 +1,24 @@
 import streamlit as st
 
-@st.cache(allow_output_mutation=True)
-def get_cached_values():
-    return []
+@st.cache_data
+def long_running_function(param1, param2):
+    # Perform time-consuming computations or data retrieval here
+    # ...
+    # Return the result
+    return result
 
 def main():
-    st.title("Caching Selected Values")
+    st.title("Long-Running Function Example")
+    
+    # Input parameters
+    param1 = st.slider("Parameter 1", 0, 10)
+    param2 = st.text_input("Parameter 2", "")
 
-    # Retrieve the cached values
-    selected_values = get_cached_values()
+    # Call the long-running function
+    result = long_running_function(param1, param2)
 
-    options = ['a', 'b', 'c', 'd', 'e']
-
-    selected_indices = st.multiselect("Select values", options, default=selected_values)
-    selected_values = [options[i] for i in selected_indices]
-
-    # Update the cached values
-    get_cached_values().clear()
-    get_cached_values().extend(selected_values)
-
-    if st.button("Print Cached Values"):
-        st.write("Cached Values:", selected_values)
+    # Display the result
+    st.write("Result:", result)
 
 if __name__ == "__main__":
     main()
