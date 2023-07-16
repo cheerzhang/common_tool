354b98120fd93c2213bbc13fc1e0ea0dd16e2660
le.zhang <le.zhang@silot.ai>
2023-07-16 17:31:12 +0200

    Update new_page.py

--- pages/new_page.py
+++ pages/new_page.py
@@ -34,7 +34,7 @@ def get_translation(token, word):
 
 def clean(word):
     pattern = r"[.,']"
-    cleaned_text = re.sub(pattern, '', word)
+    cleaned_text = re.sub(pattern, '', word.lower())
     return cleaned_text
 
 def get_unknow_word(word, voc):
