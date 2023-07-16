a41cada521c78962da68819c827f34b2913d0ea0
le.zhang <le.zhang@silot.ai>
2023-07-16 10:34:19 +0200

    Update dutch.py

--- pages/dutch.py
+++ pages/dutch.py
@@ -4,3 +4,8 @@ import pandas as pd
 st.title('Dutch Article')
 txt = st.text_area('type in the article', ' ')
 st.write(txt)
+
+
+path = 'https://raw.githubusercontent.com/cheerzhang/common_tool/main/data/dutch.csv'
+voc = pd.read_csv(path)
+st.dataframe(voc)
