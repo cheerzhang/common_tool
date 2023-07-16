ee9da087460f1e4df87560e981881ec9a21352b5
le.zhang <le.zhang@silot.ai>
2023-07-16 19:46:47 +0200

    Update dutch_read.py

--- pages/dutch_read.py
+++ pages/dutch_read.py
@@ -20,7 +20,7 @@ def get_translation(token, word):
     payload = {
 		"source_language": "nl",
 	    "target_language": "en",
-	    "text": "sluit"
+	    "text": word
 	}
     headers = {
 		"content-type": "application/x-www-form-urlencoded",
@@ -29,8 +29,8 @@ def get_translation(token, word):
 		"X-RapidAPI-Host": "text-translator2.p.rapidapi.com"
 	}
     response = requests.post(url, data=payload, headers=headers).json()
-    st.write(response)
     new_word = response['data']['translatedText']
+    st.write(new_word)
     return new_word
 
 def clean(word):
@@ -61,6 +61,7 @@ if st.button('Add all words'):
     add_df = pd.DataFrame({'word': add_words, 'translate': add_translates})
     new_df = pd.concat([voc, add_df], axis=0)
     csv_df = convert_df(new_df)
+    csv_df.to_csv(path, index=False).encode('utf-8')
     st.download_button(
       label="Download data as CSV",
       data=csv_df,
