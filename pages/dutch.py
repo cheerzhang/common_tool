0c8b8d21c4b27b12f7d123f367a36282d12aa333
le.zhang <le.zhang@silot.ai>
2023-07-16 11:52:04 +0200

    Update dutch.py

--- pages/dutch.py
+++ pages/dutch.py
@@ -20,7 +20,7 @@ def get_unknow_word(word, voc):
     word = clean(word)
     if word not in voc['word'].unique():
         st.write(f"unknown word : {word}")
-        return str("(") + str(word) + str(":xxx)")
+        return f"({word}:xxx)"
     else:
         translate = voc[voc['word'] == word]['translate'].values[0]
         st.write(f"word : {word} - {translate}")
@@ -28,7 +28,7 @@ def get_unknow_word(word, voc):
 
 str_new = ''
 for v in txt.split():
-    str_new = str_new + get_unknow_word(v, voc) + ' '
+    str_new = str_new + get_unknow_word(v, voc) + " "
 
 
 st.write(f"{str_new}")
