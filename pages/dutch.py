1751093f08d24b3bac87dffe5f166c4cf8758376
le.zhang <le.zhang@silot.ai>
2023-07-16 11:03:14 +0200

    Update dutch.py

--- pages/dutch.py
+++ pages/dutch.py
@@ -10,7 +10,16 @@ path = 'data/dutch.csv'
 voc = pd.read_csv(path)
 st.dataframe(voc)
 
+def get_unknow_word(word, voc):
+    if word.strip("'") not in voc['word'].unique():
+        st.write(f"unknown word : {word}")
+    else:
+        translate = voc[voc['word'] == word]['translate'].values[0]
+        st.write(f"word : {word} - {translate}")
 
+str_new = ''
 for v in txt.split():
-    if v.strip("'") not in voc['word'].unique():
-        st.write(f"unknown word : {v}")
+    str_new = str_new + get_unknow_word(v, voc)
+
+
+st.write(str_new)
