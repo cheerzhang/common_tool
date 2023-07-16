0a0141bf5f7404afb2f0654bbc8e415ec2cb2424
le.zhang <le.zhang@silot.ai>
2023-07-16 13:29:34 +0200

    Update new_page.py

--- pages/new_page.py
+++ pages/new_page.py
@@ -7,20 +7,19 @@ txt = st.text_area('type in the article', ' ')
 
 path = 'data/dutch.csv'
 voc = pd.read_csv(path)
-st.dataframe(voc)
 
 def clean(word):
-    pattern = r'[.,"]'
+    pattern = r"[.,']"
     cleaned_text = re.sub(pattern, '', word)
     return cleaned_text
 
 def get_unknow_word(word, voc):
-    word = clean(word)
-    if word not in voc['word'].unique():
+    word_ = clean(word)
+    if word_ not in voc['word'].unique():
         return f"({word}:xxx)"
     else:
-        translate = voc[voc['word'] == word]['translate'].values[0]
-        return f"{word}"
+        translate = voc[voc['word'] == word_]['translate'].values[0]
+        return word
 
 words = txt.split()
 str_new = ' '.join(get_unknow_word(w, voc) for w in words)
