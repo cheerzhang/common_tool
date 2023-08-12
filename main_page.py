f7747e629583b00814031d752fb8c0f9cba8776e
le.zhang <le.zhang@silot.ai>
2023-08-12 17:05:26 +0200

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -4,7 +4,7 @@ import streamlit as st
 col1, col2 = st.columns(2)
 with col1:
     st.markdown("### Tools")
-    with st.expander("gender data tools (pages/gender_data)"):
+    with st.expander("[gender data tools](pages/gender_data.py)"):
         st.markdown("Goal: Generate a dataset of first names and predict the likely gender associated with each name.")
 
 with col2:
