06c2bef7aa46ef4250f660bbe5dc078cae8bd884
le.zhang <le.zhang@silot.ai>
2023-08-12 16:55:07 +0200

    add menu page

--- data/vocabulary.py
+++
@@ -1,7 +0,0 @@
-arr_stop_word = [
-  'op', 'de', 'en', 'zoals',
-]
-arr_known_word = [
-  'organisatie', 'appartementen', 'woning', 'woningcorporatie', 'oplopen', 'heel', 'omkopen', 'vaker', 'euro','tot', 'met', 'te', 'die', 'contact','het','zo',
-  'willen','Nederland','een','om','in'
-]
--- main_page.py
+++ main_page.py
@@ -1,11 +1,10 @@
-import pandas as pd
 import streamlit as st
 
 # main 2 column
 col1, col2 = st.columns(2)
 with col1:
     st.markdown("### Tools")
-    with st.expander("gender data tools"):
+    with st.expander("gender data tools (pages/gender_data)"):
         st.markdown("Goal: Generate a dataset of first names and predict the likely gender associated with each name.")
 
 with col2:
