aefdee3c0be471bf4b810e0e483d352abc824d01
le.zhang <le.zhang@silot.ai>
2023-08-13 11:18:48 +0200

    Update gender_data.py

--- pages/gender_data.py
+++ pages/gender_data.py
@@ -2,11 +2,11 @@ import streamlit as st
 import pandas as pd
 
 def get_dataframe():
-  uploaded_file = st.file_uploader("Choose a file for training")
-  if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
-    with st.expander('original dataframe'):
-       st.dataframe(df)
+    uploaded_file = st.file_uploader("Choose a file for training")
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+        with st.expander('Original Dataframe'):
+            st.dataframe(df)
 
-def __main__():
+if __name__ == "__main__":
     get_dataframe()
