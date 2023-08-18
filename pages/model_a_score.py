9555dd9534ba63c212ba31fd73959fb5471d1b31
cheerzhang <zhanglenus@gmail.com>
2023-08-18 11:12:06 +0200

    add fe

--- pages/model_a_score.py
+++ pages/model_a_score.py
@@ -5,9 +5,17 @@ import time, datetime
 from datetime import date, timedelta
 from util import model_ml, data_util
 
+@st.cache_data
+def convert_df(df):
+    return df.to_csv(index=False).encode('utf-8')
+
 def app():
     time_tool = data_util.Time_Tool()
     st.markdown("New User Score [Fraud]")
+    d2 = st.date_input(
+        "Select created_at start date for prediction",
+        datetime.datetime.today() - timedelta(days=1)
+    )
     uploaded_fraud_file = st.file_uploader("Choose 'fraud_' csv file:", key="fraud_file_upload")
     uploaded_embedding_file_email_host = st.file_uploader("Choose '[Embedding]email_host' for embedding csv file:", key="email_host_embedding_upload")
     uploaded_embedding_file_email_server = st.file_uploader("Choose '[Embedding]email_server' for embedding csv file:", key="email_server_embedding_upload")
@@ -17,7 +25,8 @@ def app():
     and uploaded_embedding_file_email_server is not None \
     and uploaded_embedding_file_d_street is not None \
     and uploaded_embedding_file_d_city is not None:
-        df = pd.read_csv(uploaded_fraud_file)
+        obj_item = data_util.Fraud_FE()
+        df = obj_item.get_label(df, d2)
         with st.expander('get labels'):
             st.dataframe(df.head(5))
             bar_ = df.groupby(['category'])['id'].count()
@@ -30,7 +39,6 @@ def app():
             st.pyplot(fig)
             st.write(f"total data size: {df.shape[0]}, bad transcations size: {df[df['label']==1].shape[0]}")
         with st.expander('split data'):
-            obj_item = data_util.Fraud_FE()
             random_seed = st.number_input('Insert random seed', value=42, step=1)
             train_, validtest_ = obj_item.split_data(df, 'id', 0.8, random_seed)
             valid_, test_ = obj_item.split_data(validtest_, 'id', 0.5, random_seed)
@@ -40,6 +48,46 @@ def app():
             st.write(f"train size: {train_.shape}, valid size: {valid_.shape}, test size: {test_.shape}")
             st.success(f"Processed within {time_tool.end_timer()}")
         st.divider()
+        st.subheader('FE and Feature Selection for Model')
+        with st.spinner("Feature Engineering..."):
+            start_time = time.time()
+            dftr_fe = obj_item.get_features(train_)
+            dfva_fe = obj_item.get_features(valid_)
+            dfte_fe = obj_item.get_features(test_)
+            st.success(f"Processed within {time_tool.end_timer()}")
+            with st.expander("After FE, train set can be used to train embedding"):
+                st.dataframe(dftr_fe)
+            csv = convert_df(dftr_fe)
+            st.download_button(
+                label="Download the result CSV",
+                data=csv,
+                file_name='[Embedding]with_label.csv',
+                mime='text/csv',
+            )
+            st.success(f"FE for train, valid, test dataframe Done! time using: {time_tool.end_timer()}")
+        with st.spinner("Embedding processing..."):
+            time_tool.start_timer()
+            emb_fe_arr = []
+            df_emb_customer_email_host = pd.read_csv(uploaded_embedding_file_email_host)
+            df_emb_customer_email_server = pd.read_csv(uploaded_embedding_file_email_server)
+            df_emb_d_street = pd.read_csv(uploaded_embedding_file_d_street)
+            df_emb_d_city = pd.read_csv(uploaded_embedding_file_d_city)
+            emb_df = {
+                'email_host': df_emb_customer_email_host,
+                'email_server': df_emb_customer_email_server,
+                'customer_delivery_street': df_emb_d_street,
+                'customer_delivery_city': df_emb_d_city
+            }
+            obj_item = data_ml.Fraud_FE()
+            for name in emb_df.keys():
+                arr_, df_ = obj_item.map_embedding(dftr_fe, emb_df[name], name)
+                _, df_va = obj_item.map_embedding(dfva_fe, emb_df[name], name)
+                _, df_te = obj_item.map_embedding(dfte_fe, emb_df[name], name)
+                dftr_fe = df_
+                dfva_fe = df_va
+                dfte_fe = df_te
+                emb_fe_arr = emb_fe_arr + arr_
+            st.success(f"Embedding processed within {time_tool.end_timer()}")
 
 if __name__ == '__main__':
     app()
--- util/data_util.py
+++ util/data_util.py
@@ -51,3 +51,67 @@ class Fraud_FE:
         # X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.3, random_state=42)
         # X_valid, X_test, y_valid, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
         return df_train, df_valid
+    def get_features(self, df):
+        to_lower_list = ['company_name', 'customer_postcode',
+                         'customer_first_name', 'customer_last_name', 'customer_initials', 'customer_email',
+                         'customer_street', 'customer_house_extension', 'customer_city', 'customer_country_code',
+                         'customer_delivery_street', 'customer_delivery_house_extension', 'customer_delivery_city']
+        super().lower_string_for_column(df, to_lower_list)
+        # name features
+        df['first_name_len'] = df['customer_first_name'].str.len()
+        df['last_name_len'] = df['customer_last_name'].str.len()
+        df['initials_len'] = df['customer_initials'].str.len()
+        df['A_first_name'] = df['customer_first_name'].str[0:1]
+        df['A_last_name'] = df['customer_last_name'].str[0:1]
+        # 
+        df['customer_first_name'] = df['customer_first_name'].fillna('unknown')
+        df['customer_last_name'] = df['customer_last_name'].fillna('unknown')
+        df['customer_initials'] = df['customer_initials'].fillna('unknown')
+        #
+        df['initials'] = df['customer_initials'].str.replace('.', '').str.extract(r'([a-zA-Z])')[0]
+        df['same_as_first_name'] = df.apply(lambda row: 1 if row['initials'] == row['A_first_name'] else 0, axis=1)
+        # gender features
+        df['customer_sex'] = df['customer_sex'].fillna('U')
+        gender_map = {'U': 0, 'O': 0, 'unknown': 0,
+                      'M': 1, 'mr': 1,
+                      'W': 2, 'V': 2, 'F': 2, 'mrs': 2}
+        df['gender'] = df['customer_sex'].map(gender_map)
+        # age features
+        df['age'] = super().calculate_age(df, 'customer_birthdate')
+        df['birth_month'] = df['customer_birthdate'].dt.month
+        df['birth_day'] = df['customer_birthdate'].dt.day
+        df['birth_week'] = df['customer_birthdate'].dt.dayofweek
+        # email
+        df['email_host'] = df['customer_email'].str.split('.').str[-1]
+        df['email_server'] = df['customer_email'].str.split('@').str[-1].str.split('.').str[0]
+        # phone
+        # delivery address - address
+        df = super().process_typo(df, 'customer_postcode')
+        df['postcode_2'] = pd.to_numeric(df['customer_postcode'].str[0:2], errors='coerce').astype(float)
+        df['postcode_4'] = pd.to_numeric(df['customer_postcode'].str[0:4], errors='coerce').astype(float)
+        df['postcode_2str'] = 'P_'+df['customer_postcode'].str[0:2]
+        df['postcode_4str'] = 'P_'+df['customer_postcode'].str[0:4]
+        if df['customer_house_number'].dtype == 'object':
+            df['house_number'] = df['customer_house_number'].str.extract(r'(\d+)', expand=False).astype(float)
+        else:
+            df['house_number'] = df['customer_house_number']
+        df['same_as_street'] = df.apply(lambda row: 1 if row['customer_street'] == row['customer_delivery_street'] else 0, axis=1)
+        df['same_as_city'] = df.apply(lambda row: 1 if row['customer_city'] == row['customer_delivery_city'] else 0, axis=1)
+        if df['customer_delivery_house_number'].dtype == 'object':
+            df['d_house_number'] = df['customer_delivery_house_number'].str.extract(r'(\d+)', expand=False).astype(float)
+        else:
+            df['d_house_number'] = df['customer_delivery_house_number']
+        df['same_as_house_number'] = df.apply(lambda row: 1 if row['customer_house_number'] == row['customer_delivery_house_number'] else 0, axis=1)
+        df['same_as_house_extension'] = df.apply(lambda row: 1 if row['customer_house_extension'] == row['customer_delivery_house_extension'] else 0, axis=1)
+        # company information - name
+        df['client_idstr'] = 'C_' + df['client_id'].astype(str)
+        # company information - address
+        # company - size ...
+        # order amount
+        # order time
+        df['created_at'] = pd.to_datetime(df['created_at'], errors='coerce')
+        df['order_month'] = df['created_at'].dt.month
+        df['order_day']   = df['created_at'].dt.day
+        df['order_week']  = df['created_at'].dt.dayofweek
+        df['order_hour']  = df['created_at'].dt.hour
+        return df
